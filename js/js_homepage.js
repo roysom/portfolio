ea4473432e4e97d03b9e5e2d1a551cea4bf29ec4
Som <ar.somroy@gmail.com>
2019-01-09 00:25:29 +0100

    Mod: Jan2019

--- js/js_homepage.js
+++ js/js_homepage.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
     // lbuild scene
     var ourScene = new ScrollMagic.Scene({
     triggerElement: '#project6',
-    triggerHook: '0.7'
+    triggerHook: '0.8'
   })
   .setClassToggle('#project6','fade-in')
   .addTo(controller);
@@ -37,7 +37,7 @@ $(document).ready(function(){
     // lbuild scene
     var ourScene = new ScrollMagic.Scene({
     triggerElement: '#project5',
-    triggerHook: '0.7'
+    triggerHook: '0.8'
   })
   .setClassToggle('#project5','fade-in')
   .addTo(controller);
@@ -51,7 +51,7 @@ $(document).ready(function(){
     // lbuild scene
     var ourScene = new ScrollMagic.Scene({
     triggerElement: '#project4',
-    triggerHook: '0.7'
+    triggerHook: '0.8'
   })
   .setClassToggle('#project4','fade-in')
   .addTo(controller);
@@ -65,7 +65,7 @@ $(document).ready(function(){
     // lbuild scene
     var ourScene = new ScrollMagic.Scene({
     triggerElement: '#project3',
-    triggerHook: '0.7'
+    triggerHook: '0.8'
   })
   .setClassToggle('#project3','fade-in')
   .addTo(controller);
@@ -81,7 +81,7 @@ $(document).ready(function(){
     // lbuild scene
     var ourScene = new ScrollMagic.Scene({
     triggerElement: '#project2',
-    triggerHook: '0.7'
+    triggerHook: '0.8'
   })
   .setClassToggle('#project2','fade-in')
   .addTo(controller);
@@ -95,7 +95,7 @@ $(document).ready(function(){
     // lbuild scene
     var ourScene = new ScrollMagic.Scene({
     triggerElement: '#project1',
-    triggerHook: '0.7'
+    triggerHook: '0.8'
   })
   .setClassToggle('#project1','fade-in')
   .addTo(controller);
